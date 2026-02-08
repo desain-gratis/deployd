5d96f91296711bbce3213366be9866f7ff18a38b
keenan.gebze <keenan.gebze@gmail.com>
2026-02-09 06:23:44 +0700

    adjust reset script

--- restart.sh
+++ restart.sh
@@ -1,34 +1,48 @@
-ssh root@mb1 "sudo systemctl stop deployd & rm -rf /opt/deployd/data/*"
-ssh root@mb3 "sudo systemctl stop deployd & rm -rf /opt/deployd/data/*"
-ssh root@mb3 "sudo systemctl stop deployd & rm -rf /opt/deployd/data/*"
+ssh root@mb1 "sudo systemctl stop deployd"
+ssh root@mb3 "sudo systemctl stop deployd"
+ssh root@mb3 "sudo systemctl stop deployd"
 
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_1_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_1_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_1_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_2_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_2_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_2_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_3_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_3_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "deployd-v1_3_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_1_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_1_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_1_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_2_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_2_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_2_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_3_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_3_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "secretd-v1_3_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_1_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_1_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_1_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_2_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_2_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_2_3"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_3_1"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_3_2"'
-clickhouse-client -h clickhouse-darurat --password default -q 'drop database "artifactd-v1_3_3"'
+ssh root@mb1  "rm -rf /opt/deployd/data/*"
+ssh root@mb2  "rm -rf /opt/deployd/data/*"
+ssh root@mb3  "rm -rf /opt/deployd/data/*"
+
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_1_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_1_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_1_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_2_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_2_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_2_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_3_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_3_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deployd-v1_3_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_1_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_1_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_1_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_2_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_2_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_2_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_3_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_3_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "secretd-v1_3_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_1_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_1_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_1_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_2_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_2_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_2_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_3_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_3_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "artifactd-v1_3_3"'
+
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_3"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_1"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_2"'
+clickhouse-client -h clickhouse-darurat --password default -q 'drop database IF EXISTS "deploy-job-v1_4_3"'
 
 ssh root@mb1 "sudo systemctl restart deployd"
 ssh root@mb3 "sudo systemctl restart deployd"
