3e9ea1c30513c40f3abaf5fa075e831a89c003a9
K <keenan.gebze@gmail.com>
2026-02-25 03:25:22 +0700

    Update response message in main.go

--- cmd/test/user-profile/main.go
+++ cmd/test/user-profile/main.go
@@ -13,7 +13,7 @@ func main() {
 	router := httprouter.New()
 
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		fmt.Fprint(w, "Hello 世界🌍🌎\n")
+		fmt.Fprint(w, "Hello its funny its breaking things. why the service is now bound to localhost insteas of 0000\n")
 		key := "MESSAGE"
 		fmt.Fprintf(w, "Env read: %v = %v.\n", key, os.Getenv(key))
 	})
