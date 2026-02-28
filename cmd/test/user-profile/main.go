5fb5d551ab7cbe967135d0d207c1cafc05bb0155
K <keenan.gebze@gmail.com>
2026-03-01 04:05:34 +0700

    Message of za day

--- cmd/test/user-profile/main.go
+++ cmd/test/user-profile/main.go
@@ -13,7 +13,7 @@ func main() {
 	router := httprouter.New()
 
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		fmt.Fprint(w, "I hope everyone is safe 🌞🌞🌞🌞\n")
+		fmt.Fprint(w, "message of za days\n")
 		key := "MESSAGE"
 		fmt.Fprintf(w, "Env read: %v = %v.\n", key, os.Getenv(key))
 	})
