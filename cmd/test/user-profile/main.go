c6e53debca48777d0bbf88dc513671a42a1b53a6
K <keenan.gebze@gmail.com>
2026-03-01 04:00:52 +0700

    Update main.go 🌞🌞🌞🌞🌞🌞🌞

--- cmd/test/user-profile/main.go
+++ cmd/test/user-profile/main.go
@@ -13,7 +13,7 @@ func main() {
 	router := httprouter.New()
 
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		fmt.Fprint(w, "test via PR ✅✅✅\n")
+		fmt.Fprint(w, "I hope everyone is safe 🌞🌞🌞🌞\n")
 		key := "MESSAGE"
 		fmt.Fprintf(w, "Env read: %v = %v.\n", key, os.Getenv(key))
 	})
