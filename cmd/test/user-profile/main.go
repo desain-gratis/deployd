20b8f3d2d3e6e3d4b121415160affaef50e00fdc
K <keenan.gebze@gmail.com>
2026-03-04 14:16:18 +0700

    Change to 🌞🌞🌞🌞

--- cmd/test/user-profile/main.go
+++ cmd/test/user-profile/main.go
@@ -13,7 +13,7 @@ func main() {
 	router := httprouter.New()
 
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		fmt.Fprint(w, "This service is deployed using deployd ✅\n")
+		fmt.Fprint(w, "I hope everyone is safe 🌞🌞🌞🌞\n")
 		key := "MESSAGE"
 		fmt.Fprintf(w, "Env read: %v = %v.\n", key, os.Getenv(key))
 	})
